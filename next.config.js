1e0354b57bb7cba8431ca56019e42d82d4e00c5b
mako542b <marcin.piotr.konopka@gmail.com>
2023-06-10 17:44:24 +0200

    deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
     output: 'export',
-}
-
-module.exports = nextConfig
+    distDir: 'build',
+    // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+    // trailingSlash: true,
+    // Optional: Change the output directory `out` -> `dist`
+    // distDir: 'dist',
+  }
+   
+  module.exports = nextConfig
