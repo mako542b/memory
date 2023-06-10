9a355317d282fa4648b33d93ba0f8c7830cb08ec
mako542b <marcin.piotr.konopka@gmail.com>
2023-06-10 21:54:01 +0200

    images sourcepath

--- src/components/ScoreBoard.tsx
+++ src/components/ScoreBoard.tsx
@@ -10,7 +10,7 @@ export default function ScoreBoard({clicks, scored}: props) {
     return(
         <div className="grid gap-3 text-xl place-content-center text-white font-mono md:pb-3">
             <div className="flex justify-center gap-2">
-                <img src="/images/finger-click.png" alt="click" className="w-6 rounded-full"/>
+                <img src="./images/finger-click.png" alt="click" className="w-6 rounded-full"/>
                 <p className="">{clicks}</p>
             </div>
             <div className="flex justify-center">
--- src/utils/imageSources.ts
+++ src/utils/imageSources.ts
@@ -1,13 +1,13 @@
 export default function(){
     let imageSources = [
-       ['/images/dogs/dog-1.jpg', '/images/dogs/dog-2.jpg', '/images/dogs/dog-3.jpg'], 
-       ['/images/cats/cat-1.jpg', '/images/cats/cat-2.jpg', '/images/cats/cat-3.jpg'],
-       ['/images/zebra/zebra-1.jpg', '/images/zebra/zebra-2.jpg', '/images/zebra/zebra-3.jpg'],
-       ['/images/horses/horse-1.jpg', '/images/horses/horse-2.jpg', '/images/horses/horse-3.jpg'],
-       ['/images/owls/owl-1.jpg', '/images/owls/owl-2.jpg', '/images/owls/owl-3.jpg'],
-       ['/images/mouses/mouse-1.jpg', '/images/mouses/mouse-2.jpg', '/images/mouses/mouse-3.jpg'],
-       ['/images/rabbits/rabbit-1.jpg', '/images/rabbits/rabbit-2.jpg', '/images/rabbits/rabbit-3.jpg'],
-       ['/images/snakes/snake-1.jpg', '/images/snakes/snake-2.jpg', '/images/snakes/snake-3.jpg'],
+       ['./images/dogs/dog-1.jpg', './images/dogs/dog-2.jpg', './images/dogs/dog-3.jpg'], 
+       ['./images/cats/cat-1.jpg', './images/cats/cat-2.jpg', './images/cats/cat-3.jpg'],
+       ['./images/zebra/zebra-1.jpg', './images/zebra/zebra-2.jpg', './images/zebra/zebra-3.jpg'],
+       ['./images/horses/horse-1.jpg', './images/horses/horse-2.jpg', './images/horses/horse-3.jpg'],
+       ['./images/owls/owl-1.jpg', './images/owls/owl-2.jpg', './images/owls/owl-3.jpg'],
+       ['./images/mouses/mouse-1.jpg', './images/mouses/mouse-2.jpg', './images/mouses/mouse-3.jpg'],
+       ['./images/rabbits/rabbit-1.jpg', './images/rabbits/rabbit-2.jpg', './images/rabbits/rabbit-3.jpg'],
+       ['./images/snakes/snake-1.jpg', './images/snakes/snake-2.jpg', './images/snakes/snake-3.jpg'],
     ]
     return imageSources
 }
